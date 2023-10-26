d0cede47c03846771940f814757fc23f288318c8
Paiva <danpaiva@live.com>
2023-10-26 11:14:40 -0300

    feat: add model

---
+++ ColetaPertinhoAPI/Models/Ong.cs
@@ -0,0 +1,8 @@
+﻿namespace ColetaPertinhoAPI.Models;
+public class Ong
+{
+    public int OngId { get; set; }
+    public string? Nome { get; set; }
+
+    public string? LocalColeta { get; set; }
+}
