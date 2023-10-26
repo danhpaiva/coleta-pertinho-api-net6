54cfa331a10e158384c1fed0641cb4cd3147a2ea
Paiva <danpaiva@live.com>
2023-10-26 11:43:42 -0300

    feat: Modificando Entidade Principal

--- ColetaPertinhoAPI/Models/Ong.cs
+++ ColetaPertinhoAPI/Models/Ong.cs
@@ -4,6 +4,9 @@ public class Ong
     public int OngId { get; set; }
     public string? Nome { get; set; }
     public string? LocalColeta { get; set; }
+    public string? NomeResponsavel { get; set; }
+
+    public string? Email { get; set; }
     public DateTime DataCadastro { get; set; }
     public DateTime DataAtualizacao { get; set; }
     public bool Ativo { get; set; }
