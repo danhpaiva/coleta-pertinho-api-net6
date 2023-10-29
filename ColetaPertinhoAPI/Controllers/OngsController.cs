3567c8a18c4522754365d30b38c72c26f4fe3d12
Paiva <danpaiva@live.com>
2023-10-29 19:40:35 -0300

    feat: Desenvolvimento rota para retornar todas as Ong's

---
+++ ColetaPertinhoAPI/Controllers/OngsController.cs
@@ -0,0 +1,27 @@
+﻿using ColetaPertinhoAPI.Context;
+using ColetaPertinhoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ColetaPertinhoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OngsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public OngsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Ong>> Get()
+        {
+            var ongs = _context.Ongs.ToList();
+            if (ongs is null)
+                return NotFound("Ong não encontrada!");
+            return ongs;
+        }
+    }
+}
