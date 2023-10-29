eb8ab226ecb4560a8eaef39d1b3aa1747a15b55b
Paiva <danpaiva@live.com>
2023-10-29 19:45:01 -0300

    feat: Criada rota para retornar apenas uma Ong

--- ColetaPertinhoAPI/Controllers/OngsController.cs
+++ ColetaPertinhoAPI/Controllers/OngsController.cs
@@ -20,8 +20,17 @@ namespace ColetaPertinhoAPI.Controllers
         {
             var ongs = _context.Ongs.ToList();
             if (ongs is null)
-                return NotFound("Ong não encontrada!");
+                return NotFound("Nenhuma Ong encontrada!");
             return ongs;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Ong> Get(int id)
+        {
+            var ong = _context.Ongs.FirstOrDefault(o => o.OngId == id);
+            if (ong is null)
+                return NotFound($"Ong com o Id: {id} não encontrada!");
+            return ong;
         }
     }
 }
